ef55f9fbef96ee971a457555c501be1766d38cef
Hill <hildonw@dunwoody.edu>
2021-05-21 16:37:55 -0500

    what i have so far

--- routes/createAccount.js
+++ routes/createAccount.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var {check, validationResult} = require('express-validator');
+var data = require('../newUser.json'); 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
@@ -9,10 +11,27 @@ router.get('/', function(req, res, next) {
   
 });
 
-router.post('/', function(req, res){
-  var obj = JSON.stringify(req.body);
-  fs.appendFileSync('newUser.json',obj, {flag:'a'});
-  res.redirect('/create');
+router.post('/', 
+  [
+    check('password')
+    .isLength({min: 8})
+  ],
+  async(req, res) =>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      console.log(errors);
+      const alert = errors.array();
+      res.render('createAccount', alert)
+    }else{
+      var obj = req.body;
+      console.log(obj)
+      data.push(obj);
+      var updatedData = JSON.stringify(data);
+     
+      fs.writeFileSync('newUser.json',updatedData);
+      res.redirect('/create');
+    }
+    
   
 })
 
--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+const _ = require('lodash')
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
@@ -10,10 +11,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res){
-  var obj = fs.readFileSync('newUser.json');
-  obj = obj.toString('utf8')
-
-  console.log(obj)
+  var obj = fs.readFileSync('newUser.json',{encoding:'utf8', flag:'r'});
+  var pData = JSON.parse(obj);
+  console.log(pData[2])
+  for(var i = 0; i < pData.lenght; i++){
+    if((req.body.email == pData[i].email) && (req.body.password == pData[i].password)){
+      console.log(key);
+      console.log(_.isEqual(req.body.email,key));
+      res.redirect('/');
+    }
+  }
   
 })
 
